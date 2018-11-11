3c5472078ca1f818112c736f5d761e4abd254b11
Roma Kutischev <rukkola2@gmail.com>
2018-11-11 02:42:24 +0200

    seach function working

--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -3,16 +3,11 @@ import { Image, Header, List, Container, Placeholder } from "semantic-ui-react";
 
 class ClientInfo extends Component {
   render() {
-    return this.props.client ? (
-      <div>
-        <Image
-          floated="left"
-          verticalAlign="middle"
-          src={this.props.client.general.avatar}
-        />
+    return this.props.client ? <div>
+        <Image floated="left" verticalAlign="middle" src={this.props.client.general.avatar} />
         <Container text textAlign="justified">
           <Header size="huge">
-            {this.props.client.general.firstName}
+          {this.props.client.general.firstName + ' '}
             {this.props.client.general.lastName}
           </Header>
           <Header size="medium">
@@ -21,42 +16,23 @@ class ClientInfo extends Component {
             {this.props.client.job.company}
           </Header>
           <List inverted>
-            <List.Item
-              icon="phone"
-              content={
-                <a href={`callto:${this.props.client.contact.phone}`}>
+            <List.Item icon="phone" content={<a
+                  href={`callto:${this.props.client.contact.phone}`}
+                >
                   {this.props.client.contact.phone}
-                </a>
-              }
-            />
-            <List.Item
-              icon="mail"
-              content={
-                <a href={`mailto:${this.props.client.contact.email}`}>
+                </a>} />
+            <List.Item icon="mail" content={<a
+                  href={`mailto:${this.props.client.contact.email}`}
+                >
                   {this.props.client.contact.email}
-                </a>
-              }
-            />
-            <List.Item
-              icon="point"
-              content={
-                this.props.client.address.city +
-                ", " +
-                this.props.client.address.country
-              }
-            />
-            <List.Item
-              icon="address book outline"
-              content={this.props.client.address.street}
-            />
+                </a>} />
+            <List.Item icon="point" content={this.props.client.address.city + ", " + this.props.client.address.country} />
+            <List.Item icon="address book outline" content={this.props.client.address.street} />
           </List>
         </Container>
-      </div>
-    ) : (
-      <Placeholder>
+      </div> : <Placeholder>
         <Placeholder.Image rectangular />
-      </Placeholder>
-    );
+      </Placeholder>;
   }
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import { Card, Image, Input, Placeholder, Segment } from "semantic-ui-react";
 
 class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.searchClient = this.searchClient.bind(this);
+    this.state = { shownClients: null };
+  }
+
   render() {
     return (
       <div>
-        <Input icon="users" size="medium" placeholder="Search..." />
+        <Input
+          icon="users"
+          size="medium"
+          placeholder="Search..."
+          onChange={e => this.searchClient(e.target.value)}
+        />
         {this.props.isLoading ? (
           <Segment raised>
             <Placeholder>
@@ -17,25 +28,65 @@ class SideBar extends Component {
           </Segment>
         ) : (
           <Card.Group>
-            {this.props.clients.map((client, index) => (
-              <Card key={index} onClick={() => this.props.onCardClick(client)}>
-                <Card.Content>
-                  <Image
-                    floated="left"
-                    size="tiny"
-                    src={client.general.avatar}
-                  />
-                  <Card.Header>
-                    {client.general.firstName} {client.general.lastName}
-                  </Card.Header>
-                  <Card.Meta>{client.job.title}</Card.Meta>
-                </Card.Content>
-              </Card>
-            ))}
+            {this.state.shownClients
+              ? this.state.shownClients.map((client, index) => (
+                  <Card
+                    key={index}
+                    onClick={() => this.props.onCardClick(client)}
+                  >
+                    <Card.Content>
+                      <Image
+                        floated="left"
+                        size="tiny"
+                        src={client.general.avatar}
+                      />
+                      <Card.Header>
+                        {client.general.firstName} {client.general.lastName}
+                      </Card.Header>
+                      <Card.Meta>{client.job.title}</Card.Meta>
+                    </Card.Content>
+                  </Card>
+                ))
+              : this.props.clients.map((client, index) => (
+                  <Card
+                    key={index}
+                    onClick={() => this.props.onCardClick(client)}
+                  >
+                    <Card.Content>
+                      <Image
+                        floated="left"
+                        size="tiny"
+                        src={client.general.avatar}
+                      />
+                      <Card.Header>
+                        {client.general.firstName} {client.general.lastName}
+                      </Card.Header>
+                      <Card.Meta>{client.job.title}</Card.Meta>
+                    </Card.Content>
+                  </Card>
+                ))}
           </Card.Group>
         )}
       </div>
     );
+  }
+
+  searchClient(value) {
+    const shownClients = this.props.clients.filter(client => {
+      for (const property in client) {
+        for (const key in client[property]) {
+          if (
+            client[property][key].toLowerCase().startsWith(value.toLowerCase())
+          ) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+
+    this.setState({ shownClients });
+    console.log(this.state.shownClients);
   }
 }
 
